746ac4d1806c2b51ed9d220913e8b96cd5fe7140
adam.toy <atoy3731@gmail.com>
2022-09-05 21:07:06 -0400

    init script

---
+++ scripts/init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Installing Istio.."
+istioctl operator init
+kubectl apply -f ../k8s/examples/istiooperator.yaml
+sleep 5
+kubectl apply -f ../k8s/examples/gateway.yaml
+
+echo "Installing cert-manager.."
+kubectl apply -f https://github.com/jetstack/cert-manager/releases/latest/download/cert-manager.yaml
+sleep 30
+
+echo "Installing Kubewarden.."
+helm repo add kubewarden https://charts.kubewarden.io
+helm install --wait -n kubewarden --create-namespace kubewarden-crds kubewarden/kubewarden-crds
+helm install --wait -n kubewarden kubewarden-controller kubewarden/kubewarden-controller
+helm install --wait -n kubewarden kubewarden-defaults kubewarden/kubewarden-defaults
